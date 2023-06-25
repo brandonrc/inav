bd0361e7d67133f3a36aab6d7e006d35bea975e8
Brandon Geraci <brandon.geraci@gmail.com>
2023-06-25 12:33:10 -0500

    Updating VBAT scale

--- src/main/target/AXISFLYINGARGUSF7AIO/target.h
+++ src/main/target/AXISFLYINGARGUSF7AIO/target.h
@@ -121,7 +121,7 @@
 #define CURRENT_METER_ADC_CHANNEL       ADC_CHN_1
 #define VBAT_ADC_CHANNEL                ADC_CHN_2
 #define RSSI_ADC_CHANNEL                ADC_CHN_3
-#define VBAT_SCALE_DEFAULT              1600
+#define VBAT_SCALE_DEFAULT              1090
 
 #define USE_LED_STRIP
 #define WS2811_PIN                      PA8   
